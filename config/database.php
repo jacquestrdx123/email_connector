bf288920500f040d07c850c005a223d85b325aa7
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-08-02 11:18:22 +0200

    Comment

--- config/database.php
+++ config/database.php
@@ -42,6 +42,16 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGO_DB_HOST', 'localhost'),
+            'port'     => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE', 'my_database'),
+            'username' => env('MONGO_DB_USERNAME', ''),
+            'password' => env('MONGO_DB_PASSWORD', ''),
+            'options' => [
+                'database' => env('MONGO_DB_AUTHENTICATION_DATABASE', 'admin'),
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
