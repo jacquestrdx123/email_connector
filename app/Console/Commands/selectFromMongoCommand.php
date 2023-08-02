b7fcb75a15b50a6cc13c7a5f0197e2e647869163
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-08-02 18:03:55 +0200

    Comment

--- app/Console/Commands/selectFromMongoCommand.php
+++ app/Console/Commands/selectFromMongoCommand.php
@@ -8,12 +8,15 @@ use Illuminate\Console\Command;
 
 class selectFromMongoCommand extends Command
 {
-    protected $signature = 'mango:select';
+    protected $signature = 'mango:select {database}';
 
     protected $description = 'Command description';
 
     public function handle()
     {
+
+        config(['database.connections.mongodb.database' => $this->argument('database') ]);
+
         $emails = PstEmailMongo::get()->toArray();
         foreach($emails as $email){
             dd($email);
