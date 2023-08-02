250401ddda8a7a35dfb17bb9209e4287cfccd136
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-08-02 10:48:42 +0200

    Comment

---
+++ app/Models/PstEmailAttachmentMongo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PstEmailAttachmentMongo extends Model
+{
+    protected $fillable = [
+        'email_id',
+        'message_id',
+        'file_name',
+        'file_path',
+        'full_path',
+        'pst_email_attachments_tenant_id',
+    ];
+}
