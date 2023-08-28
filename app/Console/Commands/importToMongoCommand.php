1885e2fd95353e87913648725f3cf78d37961201
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-08-28 09:51:51 +0200

    Comment

--- app/Console/Commands/importToMongoCommand.php
+++ app/Console/Commands/importToMongoCommand.php
@@ -17,12 +17,12 @@ class importToMongoCommand extends Command
     public function handle()
     {
         $batchSize = 1000; // Number of emails to process per batch
-        $totalEmails = PstEmail::where('pst_name','ElmonMndawe.pst')->count();
+        $totalEmails = PstEmail::count();
         $totalBatches = ceil($totalEmails / $batchSize);
         echo "Total emails : ".$totalEmails."\n";
         for ($batch = 1; $batch <= $totalBatches; $batch++) {
             $offset = ($batch - 1) * $batchSize;
-            $emails = PstEmail::where('pst_name','ElmonMndawe.pst')->skip($offset)->take($batchSize)->get();
+            $emails = PstEmail::skip($offset)->take($batchSize)->get();
 
             foreach ($emails as $email) {
                 $mongo_email = PstEmailMongo::create($email->toArray());
