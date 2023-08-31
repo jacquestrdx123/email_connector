482e6b84254dd23fc079896e1bc4ed690667d584
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-08-31 09:19:08 +0200

    Comment

---
+++ app/Console/Commands/SearchEmails.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\PstEmail;
+use App\Models\PstEmailAttachment;
+use App\Models\PstEmailAttachmentMongo;
+use App\Models\PstEmailMongo;
+use Illuminate\Console\Command;
+
+class SearchEmails extends Command
+{
+    protected $signature = 'SearchEmails {email}';
+
+    protected $description = 'SearchEmails {email}';
+
+    public function handle()
+    {
+        $pst_emails = PstEmail::search('hello', function () {
+            // this will not be triggered
+        })->get();
+
+        return $pst_emails;
+    }
+}
