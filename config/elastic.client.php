6f1ed1355c78360769d444faedb0006d44e71567
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-08-31 08:33:32 +0200

    Comment

--- config/elastic.client.php
+++ config/elastic.client.php
@@ -5,7 +5,7 @@ return [
     'connections' => [
         'default' => [
             'hosts' => [
-                env('ELASTIC_HOST', 'localhost:9200'),
+                env('ELASTIC_HOST', '102.219.40.123:9200'),
             ],
         ],
     ],
